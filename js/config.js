0e73cb1e7f11c15ece8210c5797503a4e20f8d83
markzweigenthal <mark.zweigenthal@voomainsurance.com>
2026-04-19 14:20:36 -0400

    Update config.js
    Added google maps key

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.VOOMA_CONFIG = {
   zapierWebhookUrl: "https://hooks.zapier.com/hooks/catch/REPLACE/REPLACE/",
   calendlyUrl: "https://calendly.com/your-team/quote-review",
   canopyConnectUrl: "https://link.usecanopy.com/REPLACE",
-  googleMapsApiKey: "",
+  googleMapsApiKey: "AIzaSyDJzF_FL_jBK9oNKnwCDsnakr-Ipdbof78",
   googleAddressAutocomplete: {
     enabled: true,
     country: "us"
